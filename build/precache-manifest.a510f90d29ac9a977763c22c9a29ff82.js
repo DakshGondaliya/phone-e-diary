26b62f37650ffd364cf32c81c3f5b30f5c636f1a
DakshGondaliya <gondaliyadaksh@gmail.com>
2019-03-13 12:34:47 +0530

    dd

---
+++ build/precache-manifest.a510f90d29ac9a977763c22c9a29ff82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc117caa319a7bcaa1c8951228314e1a",
+    "url": "/phone-e-diary/static/media/phone_4.cc117caa.svg"
+  },
+  {
+    "revision": "d291d4d79e685d768296050da79df447",
+    "url": "/phone-e-diary/static/media/load_7.d291d4d7.gif"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/phone-e-diary/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/phone-e-diary/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/phone-e-diary/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "b48e527b7180f6709a23",
+    "url": "/phone-e-diary/static/js/runtime~main.4d8cfd1f.js"
+  },
+  {
+    "revision": "8c17d164006a9b6aab1b",
+    "url": "/phone-e-diary/static/js/main.e261fb05.chunk.js"
+  },
+  {
+    "revision": "338c3806e1ad573d20d4",
+    "url": "/phone-e-diary/static/js/2.09deac6d.chunk.js"
+  },
+  {
+    "revision": "8c17d164006a9b6aab1b",
+    "url": "/phone-e-diary/static/css/main.ee823990.chunk.css"
+  },
+  {
+    "revision": "d7f71393f31efff9c3fbe05c58a39d93",
+    "url": "/phone-e-diary/index.html"
+  }
+];
